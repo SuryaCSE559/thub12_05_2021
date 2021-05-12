57661f9c2ac7637d562ac141a144ebb9c2b6bd59
SuryaCSE559 <84056644+SuryaCSE559@users.noreply.github.com>
2021-05-12 09:51:43 +0530

    Update prime1.cpp

--- prime1.cpp
+++ prime1.cpp
@@ -6,12 +6,12 @@ int main()
 	cin>>n;
 	for(i=2;i<n;i++)
 	{
-		if(n%i==0)
-            flag=1;
-            break;
+	    if(n%i==0)
+               flag=1;
+               break;
 	}
 	if(flag==1)
 	   cout<<"Its not a prime";
 	else
-	   cout<<"Its a prime97";
+	   cout<<"Its a prime";
 }
