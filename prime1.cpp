3ffb75019a8883b1563059f9ab6ccf40059c48bf
SuryaCSE559 <84056644+SuryaCSE559@users.noreply.github.com>
2021-05-12 09:48:30 +0530

    Add files via upload

---
+++ prime1.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n,i,flag=0;
+	cin>>n;
+	for(i=2;i<n;i++)
+	{
+		if(n%i==0)
+            flag=1;
+            break;
+	}
+	if(flag==1)
+	   cout<<"Its not a prime";
+	else
+	   cout<<"Its a prime97";
+}
